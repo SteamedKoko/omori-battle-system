612580dd9e57050c5c3a31a77ae69221d6b8657b
SteamedKoko <SteamedKoko@gmail.com>
2026-03-23 18:05:38 +0900

    refactor: fix damage fading

--- Scenes/Battle/UI/player_panel.gd
+++ Scenes/Battle/UI/player_panel.gd
@@ -8,6 +8,7 @@ const PLAYER_PANEL = preload("uid://cfoptwsymul31")
 @export var layout_position: Control.LayoutPreset
 @export var player_data: PlayerData
 @export var stats: Stats
+@export var damage_fade_time: float = .2
 @export var length_to_show_damage: float = 1
 
 @onready var player_box: TextureRect = %PlayerBox
@@ -22,6 +23,7 @@ const PLAYER_PANEL = preload("uid://cfoptwsymul31")
 @onready var juice_max_text: RichTextLabel = %JuiceTextMax
 @onready var juice_current_text: RichTextLabel = %JuiceTextCurrent
 @onready var damage_text: Label = %DamageNumber
+@onready var damage_container: PanelContainer = %DamageContainer
 
 var sprite_frames: SpriteFrames
 var sprite_state: SpriteStates:
@@ -47,7 +49,7 @@ enum SpriteStates {
 func _ready():
 	_set_panel_location(layout_position)
 
-	damage_text.self_modulate = Color.TRANSPARENT
+	damage_container.modulate = Color.TRANSPARENT
 
 	health_bar.max_value = stats.max_hp
 	juice_bar.max_value = stats.max_juice
@@ -73,9 +75,10 @@ func show_damage(amount: int) -> void:
 	var tween: Tween = get_tree().create_tween()
 	damage_text.text = str(amount)
 	damage_text.self_modulate = Color.WHITE
-	tween.tween_property(damage_text, "self_modulate", Color.WHITE, 1)
-	tween.tween_property(damage_text, "self_modulate", Color.TRANSPARENT, 1)
-	await get_tree().create_timer(length_to_show_damage).timeout
+	tween.tween_property(damage_container, "modulate:a", 1, damage_fade_time)
+	tween.tween_interval(length_to_show_damage)
+	tween.tween_property(damage_container, "modulate:a", 0, damage_fade_time)
+	await tween.finished
 
 func _update_sprite_state(value: SpriteStates):
 	if value == SpriteStates.NEUTRAL:
